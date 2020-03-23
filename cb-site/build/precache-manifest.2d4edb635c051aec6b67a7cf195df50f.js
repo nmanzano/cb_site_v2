967b9097c20a03210aaf81c95ad7a9b4341a2878
Noni <noni.manzano@gmail.com>
2020-03-22 22:39:53 -0500

    changes

---
+++ cb-site/build/precache-manifest.2d4edb635c051aec6b67a7cf195df50f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e5f14e3a2bf0d4db0cc0ef69cdc634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a911d6a692c440162db2",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d6b0a572ef629385ed85",
+    "url": "/static/css/main.f8d2ffbb.chunk.css"
+  },
+  {
+    "revision": "a911d6a692c440162db2",
+    "url": "/static/js/2.33539a94.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.33539a94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6b0a572ef629385ed85",
+    "url": "/static/js/main.1e4f26a0.chunk.js"
+  },
+  {
+    "revision": "2ec3f32dfde46e77a477",
+    "url": "/static/js/runtime-main.0bb35b82.js"
+  },
+  {
+    "revision": "94e9584a2fd180a76be40c5a1e44be34",
+    "url": "/static/media/ProjectProposal_phases.94e9584a.png"
+  },
+  {
+    "revision": "490a9bc76a3186b7b860d17d4b5b497a",
+    "url": "/static/media/ProjectProposal_staar.490a9bc7.png"
+  },
+  {
+    "revision": "4d16582637f8e4cabe95ca5df1aa58db",
+    "url": "/static/media/addie_model.4d165826.png"
+  },
+  {
+    "revision": "242fb840f16696763b85f94ba4ed0d11",
+    "url": "/static/media/contact_picture.242fb840.jpg"
+  },
+  {
+    "revision": "82d12e44ca7a081a1645137e45d9ea8c",
+    "url": "/static/media/headshot.82d12e44.jpg"
+  },
+  {
+    "revision": "f4717aa951fe3d5a0e78485a8c580a95",
+    "url": "/static/media/job_description.f4717aa9.png"
+  },
+  {
+    "revision": "26bd020633a1375c5ff69ab2aa1f7e02",
+    "url": "/static/media/jobaids.26bd0206.png"
+  },
+  {
+    "revision": "580ddc862f5e5c8a9bbe21b557e29f23",
+    "url": "/static/media/portfolio_curriculum_writing.580ddc86.png"
+  },
+  {
+    "revision": "4c3a05f97cec650ebb3f2630e3d458c7",
+    "url": "/static/media/portfolio_examples.4c3a05f9.png"
+  },
+  {
+    "revision": "b82bb435edf458c83bcc66a33ab0c14e",
+    "url": "/static/media/portfolio_instructional_design.b82bb435.png"
+  },
+  {
+    "revision": "b50fe3395ce1f78b05b40cd2bd0387d1",
+    "url": "/static/media/portfolio_prassessment.b50fe339.png"
+  },
+  {
+    "revision": "5ab64fa54f3ff3e07baea6e9b1c6ec78",
+    "url": "/static/media/portfolio_video.5ab64fa5.png"
+  },
+  {
+    "revision": "e35131ec1cafb623dfa9127af1ba1fd2",
+    "url": "/static/media/three_instructional_archs.e35131ec.png"
+  },
+  {
+    "revision": "8c70de3fda9fb21f75e18d3a938c3236",
+    "url": "/static/media/user_manual_continue.8c70de3f.png"
+  },
+  {
+    "revision": "aa2e84b0bf547788fb45f30cba324fe1",
+    "url": "/static/media/usermanual_intro.aa2e84b0.png"
+  }
+]);
